3ee8ab51f1016f45c68dae5f32579745a2e9ba0e
Ewan <ewann@users.noreply.github.com>
2024-06-17 17:06:42 +0100

    Update gpt4all.rb

--- Casks/gpt4all.rb
+++ Casks/gpt4all.rb
@@ -1,6 +1,6 @@
 cask "gpt4all" do
 	version "2.4.6"
-	sha256 "1f3dbc28ead31b562402d2786f77bb9cd85291a8be2dbf8c0fcb85d4d438410e"
+	sha256 "37551b3f1b51bc78a7a49cc4d6b9f5ecee08d165eefb0775131a0ba1a216aade"
 	url "https://gpt4all.io/installers/gpt4all-installer-darwin.dmg"
 	name "GPT4All"
 	desc "gpt4all: an ecosystem of open-source chatbots trained on a massive collections of clean assistant data including code, stories and dialogue"
